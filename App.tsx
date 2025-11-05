41f4e766b62104f1918fa660e049f0c2d0612055
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2025-11-05 10:02:36 -0300

    feat: request notification permission for OneSignal integration

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.initialize("1905ae86-2e20-4b51-a268-6eee8bb717ea");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
